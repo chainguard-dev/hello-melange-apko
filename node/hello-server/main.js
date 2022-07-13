bd65155f921babf7faf8fb9f36dd41fc8149b336
Josh Dolitsky <josh@dolit.ski>
2022-07-12 22:07:43 -0500

    (node/hello-server): initial version
    Signed-off-by: Josh Dolitsky <josh@dolit.ski>

---
+++ node/hello-server/main.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Example app listening on port ${port}`)
+})
